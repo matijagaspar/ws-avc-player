1ae3b6e3d4c933f2dfced0789a4fb7584b9b9795
Matija Gaspar <matija.gaspar@gmail.com>
2017-11-26 18:30:10 +0100

    up the version, window example

--- example/index.js
+++ example/index.js
@@ -81,5 +81,11 @@ server.listen(8080)
 // ffmpeg OSX
 // then run ffmpeg: ffmpeg -framerate 30 -video_size 640x480 -f avfoundation -i 0  -vcodec libx264 -vprofile baseline -b:v 500k -bufsize 600k -tune zerolatency -pix_fmt yuv420p -r 15 -g 30 -f rawvideo tcp://localhost:5000
 
+// fmpeg Windows:
+
+// ffmpeg -framerate 25 -video_size 640x480 -f dshow -i "video=<DEVICE>"  -vcodec libx264 -vprofile baseline -b:v 500k -bufsize 600k -tune zerolatency -pix_fmt yuv420p -f rawvideo tcp://localhost:5000
+// to get video devices run:
+// ffmpeg -list_devices true -f dshow -i dummy
+
 // RPI
 // /opt/vc/bin/raspivid -pf baseline -ih -t 0 -w 640 -h 480 -hf -fps 15 -g 30 -o - | nc localhost 5000
